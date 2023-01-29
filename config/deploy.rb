6256bb9e2665826e66fb1fac9090e4c7b0379928
sfsf700 <hirokazu.2rnk@gmail.com>
2023-01-30 01:39:57 +0900

    deploy2

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :application, 'mocha'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:sfsf700/mocha.git'
 
+set :branch, "main"
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
