1fd036378ca88f69a99daa9f92cbcd89f369195c
sfsf700 <hirokazu.2rnk@gmail.com>
2023-02-01 20:28:42 +0900

    投稿表示順変更3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,9 +2,8 @@ class BlogsController < ApplicationController
   
 
   def index
-    @blogs = Blog.page(params[:page]).per(10)
-    #@blogs = Blog.order("created_at DESC").params[:page].per(5)
-  end
+    #@blogs = Blog.page(params[:page]).per(10)
+    @blogs = Blog.order("created_at DESC").params[:page].per(5)
   end
 
   def show
